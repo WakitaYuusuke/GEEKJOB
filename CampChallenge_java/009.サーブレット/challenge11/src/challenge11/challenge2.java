a05f45d8c807d7b32747be6c14718f4118e2ca9b
DESKTOP-560887D\Yusuke <yuusuke5815@gmail.com>
2017-11-09 14:40:29 +0900

    Java 　クラスの継承の課題です

---
+++ CampChallenge_java/009.サーブレット/challenge11/src/challenge11/challenge2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package challenge11;
+
+/**
+ *
+ * @author Yusuke
+ */
+public class challenge2 extends Kadai {
+    public void clearNum(){
+       a = 0;
+       b = 0;
+    }
+}
